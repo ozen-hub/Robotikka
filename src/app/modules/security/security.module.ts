d21c49fffb70dd26419ba671e9efa970b2f64aec
ozen-hub <info.ozenhub@gmail.com>
2022-12-29 20:35:18 +0530

    5

--- src/app/modules/security/security.module.ts
+++ src/app/modules/security/security.module.ts
@@ -5,6 +5,9 @@ import { SecurityRoutingModule } from './security-routing.module';
 import { SecurityComponent } from './security.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
 
 
 @NgModule({
@@ -15,7 +18,10 @@ import { SignupComponent } from './components/signup/signup.component';
   ],
   imports: [
     CommonModule,
-    SecurityRoutingModule
+    SecurityRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
   ]
 })
 export class SecurityModule { }
