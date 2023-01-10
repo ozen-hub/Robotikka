865847e83643edeb69470878fabffc780f5faaa3
ozen-hub <info.ozenhub@gmail.com>
2023-01-10 21:04:16 +0530

    10

--- src/app/modules/console/console.module.ts
+++ src/app/modules/console/console.module.ts
@@ -6,9 +6,8 @@ import { ConsoleComponent } from './console.component';
 import { DashboardContextComponent } from './inner-items/dashboard-context/dashboard-context.component';
 import { ManageProductComponent } from './inner-items/dashboard-context/items/manage-product/manage-product.component';
 import { MainConsoleHeaderComponent } from './components/main-console-header/main-console-header.component';
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {MatInputModule} from "@angular/material/input";
-import {FormsModule} from "@angular/forms";
+
+
 
 
 @NgModule({
@@ -20,9 +19,7 @@ import {FormsModule} from "@angular/forms";
   ],
   imports: [
     CommonModule,
-    ConsoleRoutingModule,
-    MatFormFieldModule,
-    MatInputModule
+    ConsoleRoutingModule
   ]
 })
 export class ConsoleModule { }
