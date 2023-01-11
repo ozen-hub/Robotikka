3ba32201e41f89978ed57e3414b8b1f804d3d805
ozen-hub <info.ozenhub@gmail.com>
2023-01-11 21:15:19 +0530

    13

--- src/app/modules/security/components/signup/signup.component.ts
+++ src/app/modules/security/components/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {UserService} from "../../../share/services/user/user.service";
 
 @Component({
   selector: 'app-signup',
@@ -8,11 +9,12 @@ import {FormControl, FormGroup, Validators} from "@angular/forms";
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   registerForm= new FormGroup({
     firstName: new FormControl(null,[Validators.required]),
     lastName: new FormControl(null,[Validators.required]),
+    contact: new FormControl(null,[Validators.required]),
     email: new FormControl(null,[Validators.required,Validators.email]),
     password: new FormControl(null,[Validators.required]),
   })
@@ -21,6 +23,16 @@ export class SignupComponent implements OnInit {
   }
 
   signup() {
+
+    this.userService.register(
+      this.registerForm.get('firstName')?.value!,
+      this.registerForm.get('lastName')?.value!,
+      this.registerForm.get('contact')?.value!,
+      this.registerForm.get('email')?.value!,
+      this.registerForm.get('password')?.value!,
+    ).subscribe(response=>{
+      console.log(response);
+    })
 
   }
 }
---
+++ src/app/modules/share/services/user/user.service.ts
@@ -0,0 +1,24 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import * as http from "http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  public register(firstName: string, lastName: string, contact: string,
+                  email: string, password: string): Observable<any> {
+    return this.http.post('http://localhost:8000/api/v1/users/visitor/register', {
+      firstName: firstName,
+      lastName: lastName,
+      contact: contact,
+      email: email,
+      password: password,
+    })
+  }
+}
