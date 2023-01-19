0c255ec1a10d1e905a3372b5b7f48718bc1a06d3
ozen-hub <info.ozenhub@gmail.com>
2023-01-19 10:40:18 +0530

    19

--- src/app/modules/console/inner-items/dashboard-context/items/manage-product/delete-product/delete-product.component.ts
+++ src/app/modules/console/inner-items/dashboard-context/items/manage-product/delete-product/delete-product.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SnackBarService} from "../../../../../../share/services/core/snack-bar.service";
+import {ProductService} from "../../../../../../share/services/product/product.service";
+import {FormControl, FormGroup, FormGroupDirective, Validators} from "@angular/forms";
+import ProductDTO from "../../../../../../share/dto/ProductDTO";
 
 @Component({
   selector: 'app-delete-product',
@@ -7,9 +11,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeleteProductComponent implements OnInit {
 
-  constructor() { }
+  constructor(private snackbarService: SnackBarService, private productService: ProductService) {
+  }
+
+  form = new FormGroup({
+    productId: new FormControl('', [Validators.required]),
+    displayName: new FormControl(''),
+    description: new FormControl(''),
+    unitPrice: new FormControl(''),
+    qty: new FormControl(''),
+    sellingPrice: new FormControl(''),
+  })
 
   ngOnInit(): void {
   }
+
+  findProduct() {
+    if (this.form.get('productId')?.value!) {
+      this.productService.findProduct(this.form.get('productId')?.value!).subscribe(response => {
+        if (response.code === 200) {
+          this.form.patchValue({
+            displayName: response.data.displayName,
+            description: response.data.description,
+            unitPrice: response.data.unitPrice,
+            qty: response.data.qty,
+            sellingPrice: response.data.sellingPrice
+          })
+        }
+      }, error => {
+        this.snackbarService.showSnackbar('Not Found', 'Close');
+      })
+    } else {
+      this.snackbarService.showSnackbar('Please enter the id', 'Close');
+    }
+  }
+
+  deleteProduct(formData: FormGroupDirective) {
+    this.productService.deleteProduct(this.form.get('productId')?.value!).subscribe(response => {
+      this.snackbarService.showSnackbar('Success!', 'Close');
+      console.log(response)
+      this.refreshForm(formData);
+    }, error => {
+      this.snackbarService.showSnackbar('Something went wrong', 'Close');
+    })
+  }
+
+  private refreshForm(formData: FormGroupDirective) {
+    formData.resetForm();
+  }
+
 
 }
--- src/app/modules/share/services/product/product.service.ts
+++ src/app/modules/share/services/product/product.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import ProductDTO from "../../dto/ProductDTO";
@@ -8,28 +8,34 @@ import ProductDTO from "../../dto/ProductDTO";
 })
 export class ProductService {
 
-  constructor(private http: HttpClient) { }
-
-  public createProduct(product: ProductDTO):Observable<any>{
-    return this.http.post('http://localhost:8000/api/v1/products/member/create',{
-      displayName:product.displayName,
-      description:product.description,
-      unitPrice:product.unitPrice,
-      qty:product.qty,
-      sellingPrice:product.sellingPrice,
-    })
-  }
-  public findProduct(id: string):Observable<any>{
-    return this.http.get('http://localhost:8000/api/v1/products/member/find/'+id);
+  constructor(private http: HttpClient) {
   }
 
-  updateProduct(product: ProductDTO, id: string):Observable<any>{
-    return this.http.put('http://localhost:8000/api/v1/products/member/modify/'+id,{
-      displayName:product.displayName,
-      description:product.description,
-      unitPrice:product.unitPrice,
-      qty:product.qty,
-      sellingPrice:product.sellingPrice,
+  public createProduct(product: ProductDTO): Observable<any> {
+    return this.http.post('http://localhost:8000/api/v1/products/member/create', {
+      displayName: product.displayName,
+      description: product.description,
+      unitPrice: product.unitPrice,
+      qty: product.qty,
+      sellingPrice: product.sellingPrice,
     })
+  }
+
+  public findProduct(id: string): Observable<any> {
+    return this.http.get('http://localhost:8000/api/v1/products/member/find/' + id);
+  }
+
+  updateProduct(product: ProductDTO, id: string): Observable<any> {
+    return this.http.put('http://localhost:8000/api/v1/products/member/modify/' + id, {
+      displayName: product.displayName,
+      description: product.description,
+      unitPrice: product.unitPrice,
+      qty: product.qty,
+      sellingPrice: product.sellingPrice,
+    })
+  }
+
+  deleteProduct(id: string): Observable<any> {
+    return this.http.delete('http://localhost:8000/api/v1/products/member/remove/' + id);
   }
 }
